c68893339bf000ca26873c18896aec73c15690c0
tuandh12345 <hoangtuan97531@gmail.com>
2023-07-10 08:19:50 +0700

    Update

--- InventorySoftware/src/com/raven/form/Form_Home.java
+++ InventorySoftware/src/com/raven/form/Form_Home.java
@@ -124,7 +124,7 @@ public class Form_Home extends javax.swing.JPanel {
 
         jLabel1.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(4, 72, 210));
-        jLabel1.setText("Dashboard / Home");
+        jLabel1.setText("Home");
 
         card2.setBackground(new java.awt.Color(10, 30, 214));
         card2.setColorGradient(new java.awt.Color(72, 111, 252));
