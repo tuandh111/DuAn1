38c7e7845f0e78e911f6a19b087336e17cd96dd1
bambiiiiiiiii <nguyenquocthang913@gmail.com>
2023-11-28 15:27:53 +0700

    FirstBlood

--- InventorySoftware/src/nhom2/test/thang/KhachHangTest.java
+++ InventorySoftware/src/nhom2/test/thang/KhachHangTest.java
@@ -1,5 +1,27 @@
 package nhom2.test.thang;
 
-public class KhachHangTest {
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.DuAn1.Dao.KhachHangDAO;
+import com.DuAn1.Model.KhachHangModel;
+
+public class KhachHangTest {
+	KhachHangDAO dao = new KhachHangDAO();
+	String kq = "";
+	String kqmd = "Thành Công";
+
+	@Test
+	public void test() {
+		List<KhachHangModel> list = dao.selectMa("KH00001");
+		if (list != null) {
+			kq = "Thành Công";
+		} else {
+			kq = "Thất Bại";
+		}
+		Assert.assertEquals(kq, kqmd);
+
+	}
 }
